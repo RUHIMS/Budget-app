0b75e8f93eff2c999b9e2ab24ecc74807985e318
Ruhi <ruhi.kumari@metricstream.com>
2019-02-11 14:50:22 +0530

    Removed Dummy Expense Data

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import './App.scss';
 
-
-
-const store = configureStore();
-store.dispatch(addExpense({description:'Water bill', amount:'450'}));
+/*store.dispatch(addExpense({description:'Water bill', amount:'450'}));
 store.dispatch(addExpense({description:'Electricity bill',amount:'500',createdAt: '10000'}));
 store.dispatch(addExpense({description:'Garbage bill',amount:'40',createdAt:'100'}));
 store.dispatch(addExpense({description:'House Rent',amount:'1300'}));
@@ -25,8 +22,8 @@ store.dispatch(setTextFilter('bill'));
 
 const state=store.getState();
 const VisibleExpenses=getVisibleExpenses(state.expenses,state.filters);
-console.log(VisibleExpenses);
-
+console.log(VisibleExpenses);*/
+const store = configureStore();
 const jsx=(<Provider store={store}>
 <AppRouter />
 </Provider>)
