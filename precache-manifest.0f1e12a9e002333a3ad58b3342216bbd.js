073405cf7629db26e57e8542c6f1ed5a7ab97b63
Ruhi <ruhi.kumari@metricstream.com>
2019-02-17 09:42:44 +0530

    Updates

---
+++ precache-manifest.0f1e12a9e002333a3ad58b3342216bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d789f6403ceefc706a3a",
+    "url": "/Budget-app/static/js/runtime~main.d789f640.js"
+  },
+  {
+    "revision": "58d0a48cee7b1a124e5d",
+    "url": "/Budget-app/static/js/main.58d0a48c.chunk.js"
+  },
+  {
+    "revision": "10d581d0576a4ce27571",
+    "url": "/Budget-app/static/js/1.10d581d0.chunk.js"
+  },
+  {
+    "revision": "58d0a48cee7b1a124e5d",
+    "url": "/Budget-app/static/css/main.a3dec6df.chunk.css"
+  },
+  {
+    "revision": "10d581d0576a4ce27571",
+    "url": "/Budget-app/static/css/1.29af4e9c.chunk.css"
+  },
+  {
+    "revision": "5f60f03fef840fe13d8686ccf86c2cff",
+    "url": "/Budget-app/index.html"
+  }
+];
